b50695ea76a74d82830751019feb2eaf3f97762b
daiyi <clsun91@gmail.com>
2015-06-06 16:03:43 -0700

    organised project, encasulated state

--- mazerunner.js
+++ mazerunner.js
@@ -9,10 +9,6 @@ var data = {
     [1, 0, 1, 0, 1, 0, 1, 0, 0],
     [1, 0, 1, 0, 0, 0, 1, 0, 0],
     [0, 0, 1, 0, 1, 0, 1, 0, 0]
-  ],
-  'testGrid': [
-    [1,0],
-    [0,0]
   ]
 }
 
@@ -20,113 +16,209 @@ var TEAL = 0x28AFAA;
 var DARK_TEAL = 0x007470;
 var ORANGE = 0xFF933D;
 
-var SCALE = 80;
-var SPEED = 300;  // movement tweening in ms
+function MazeGame(grid) {
+  this.grid = grid;
+  this.player = new Player();
 
-// TODO organise these into objects
-var graphics, walls, player, cursors;
-var maze = data['grid'];
-var playerGrid = {'x': 0, 'y': 0};
-var status = true; // false = gameover
-var lastTimestep = null, stationary = true;
-// debug
-var savePlayerGrid = savePlayer = null;
+  this.SCALE = 80;
+  this.SPEED = 300;  // movement tweening in ms
 
-var game = new Phaser.Game(SCALE * maze.length, SCALE * maze[0].length, Phaser.AUTO, 'game', { preload: preload, create: create, update: update });
+  this.walls;
+  this.cursors;
+  this.isPlaying = true;
 
+  this.preload = function () {
+      this.game.load.image('salty', 'assets/salty.png');
+      // this.load.image('rock', 'assets/rock.png');
+      this.game.load.spritesheet('rock', 'assets/rock.png', 100,100,2);
+      this.addEvent(document.querySelector('#btn-run'), 'click', this.runMaze);
+      this.addEvent(document.querySelector('#btn-restart'), 'click', this.restart);
+      // this.addEvent(document.querySelector('#btn-theme-rocks'), 'click', this.setTheme(this.walls, 0));
+      // this.addEvent(document.querySelector('#btn-theme-rocks2'), 'click', this.setTheme(this.walls, 1));
+  }.bind(this);
 
-function preload() {
-  game.load.image('salty', 'assets/salty.png');
-  game.load.image('rock', 'assets/rock.png');
+  this.create = function () {
+    var graphics = this.game.add.graphics(0, 0);
 
-}
+    // draw background
+    graphics.beginFill(TEAL);
+    graphics.drawRect(0, 0 ,this.SCALE * this.grid.length, this.SCALE * this.grid[0].length);
 
-function create() {
-
-  graphics = game.add.graphics(0, 0);
-
-  // draw background
-  graphics.beginFill(TEAL);
-  graphics.drawRect(0, 0 ,SCALE * maze.length, SCALE * maze[0].length);
-
-  walls = game.add.group();
-  for (var gridY = 0; gridY < maze.length; gridY++) {
-    for (var gridX = 0; gridX < maze.length; gridX++) {
-      if (maze[gridY][gridX]) {
-        var wall = walls.create(gridX * SCALE, gridY * SCALE, 'rock');
-        wall.scale.setTo(SCALE/wall.width, SCALE/wall.height);
-      }
-      else if (gridX == 0 && !player) {
-        player = game.add.sprite(gridX * SCALE, gridY * SCALE, 'salty');
-        player.scale.setTo(SCALE/player.width, SCALE/player.height);
-        player.anchor.setTo(0, 0);
-        playerGrid.x = gridX;
-        playerGrid.y = gridY;
-      }
-    }
-  }
-
-
-
-  //  keyboard controls
-  cursors = game.input.keyboard.createCursorKeys();
-}
-
-function update() {
-  if (status) {
-    debug();
-
-    if (stationary) {
-      if (cursors.left.isDown && playerGrid.x > 0 && maze[playerGrid.y][playerGrid.x-1] != 1) {
-        move({x: player.x - SCALE, y: player.y});
-        playerGrid.x -= 1;
-      }
-      else if (cursors.right.isDown && playerGrid.x < maze.length-1  && maze[playerGrid.y][playerGrid.x+1] != 1) {
-        move({x: player.x + SCALE, y: player.y});
-        playerGrid.x += 1;
-      }
-      else if (cursors.up.isDown && playerGrid.y > 0  && maze[playerGrid.y-1][playerGrid.x] != 1) {
-        move({x: player.x, y: player.y - SCALE});
-        playerGrid.y -= 1;
-      }
-      else if (cursors.down.isDown && playerGrid.y < maze.length-1  && maze[playerGrid.y+1][playerGrid.x] != 1) {
-        move({x: player.x, y: player.y + SCALE});
-        playerGrid.y += 1;
+    this.walls = this.game.add.group();
+    for (var y = 0; y < this.grid.length; y++) {
+      for (var x = 0; x < this.grid.length; x++) {
+        if (this.grid[y][x] === 1) {
+          var wall = this.walls.create(x * this.SCALE, y * this.SCALE, 'rock');
+          wall.frame = 0;
+          wall.scale.setTo(this.SCALE/wall.width, this.SCALE/wall.height);
+        }
+        else if (x == 0 && this.player.sprite === undefined) {
+          this.player.sprite = this.game.add.sprite(x * this.SCALE, y * this.SCALE, 'salty');
+          this.player.sprite.scale.setTo(this.SCALE/this.player.sprite.width, this.SCALE/this.player.sprite.height);
+          this.player.sprite.anchor.setTo(0, 0);
+          this.player.startLocation = new Coordinate(x, y);
+          this.player.location = new Coordinate(x,y);
+        }
       }
     }
 
+    //  keyboard controls
+    this.cursors = this.game.input.keyboard.createCursorKeys();
+  }.bind(this);
+
+  this.update = function () {
+    if (this.isPlaying) {
+      if (!this.player.isMoving) {
+        if (this.cursors.left.isDown && this.check('left', 0)) {
+          this.movePlayer('left');
+        }
+        else if (this.cursors.right.isDown && this.check('right', 0)) {
+          this.movePlayer('right');
+        }
+        else if (this.cursors.up.isDown && this.check('up', 0)) {
+          this.movePlayer('up');
+        }
+        else if (this.cursors.down.isDown && this.check('down', 0)) {
+          this.movePlayer('down');
+        }
+      }
+
+    }
+    else {
+      // game over
+    }
+  }.bind(this);
+
+  this.movePlayer = function (direction) {
+    var newCoord;
+    switch (direction) {
+      case 'up':
+        this.player.location.increment(Coordinate.prototype.direction.up);
+        break;
+      case 'down':
+        this.player.location.increment(Coordinate.prototype.direction.down);
+        break;
+      case 'left':
+        this.player.location.increment(Coordinate.prototype.direction.left);
+        break;
+      case 'right':
+        this.player.location.increment(Coordinate.prototype.direction.right);
+        break;
+    }
+    coord = this.player.location;
+    console.log(this.player.sprite.x, this.player.sprite.y);
+    this.player.isMoving = true;
+
+    this.game.add.tween(this.player.sprite).to({'x': coord.x * this.SCALE, 'y': coord.y * this.SCALE}, this.SPEED, Phaser.Easing.Quadratic.InOut,  true).onComplete.add(function() { this.player.isMoving = false;}, this);
+  };
+
+  this.runMaze = function () {
+    console.log('RUNNING MAZE');
+    // path = [new Coordinate(1,8), new Coordinate(1,7), new Coordinate(1,6)];
+
+  };
+
+  this.restart = function () {
+    if (this.player.startLocation != undefined) {
+      this.player.sprite.x = this.player.startLocation.x * this.SCALE;
+      this.player.sprite.y = this.player.startLocation.y * this.SCALE;
+      this.player.location.x = this.player.startLocation.x;
+      this.player.location.y = this.player.startLocation.y;
+    }
+  }.bind(this);
+
+  // this.setTheme = function (group, frameNumber) {
+  //   group.children.forEach(function(sprite){
+  //     sprite.frame = frameNumber;
+  //   });
+  // }.bind(this);
+
+  this.mazeToGraph = function (maze) {
+    var start = new Coordinate(null, null);
+    var finish = new Coordinate(null, null);
+    var graph = {start: {}, finish: {}};
+    var toVisit = [];
+
+    for (var y=0; y < maze.length; y++) {
+      if (maze[y][0] === 0) {
+        var p = new Coordinate(0, y);
+        toVisit.push(p);
+
+        // connect coordinates in leftmost column to start node
+        graph.start[p] = 0;
+      }
+    }
+
+    return graph;
   }
-  else {
-    // game over
+
+  createGame = function () {
+    var game = new Phaser.Game(this.SCALE * this.grid.length, this.SCALE * this.grid[0].length, Phaser.AUTO, 'game', { preload: this.preload, create: this.create, update: this.update });
+    return game;
+  }.bind(this);
+
+  this.game = createGame();
+  // this.graph = mazeToGraph(this.grid);
+}
+MazeGame.prototype.check = function (direction, val) {
+  x = this.player.location.x;
+  y = this.player.location.y;
+  switch (direction) {
+    case 'up':
+      y -= 1;
+      break;
+    case 'down':
+      y += 1;
+      break;
+    case 'left':
+      x -= 1;
+      break;
+    case 'right':
+      x += 1;
+      break;
   }
+  if (val !== undefined) {
+    return (0 < x) && (x < this.grid.length) && (0 < y) && (y < this.grid.length) && (this.grid[y][x] === val);
+  }
+  return (0 < x) && (x < this.grid.length) && (0 < y) && (y < this.grid.length);
+};
+MazeGame.prototype.addEvent = function (element, evnt, funct) {
+  // IE9 and below
+  if (element.attachEvent)
+   return element.attachEvent('on'+evnt, funct);
+  else
+   return element.addEventListener(evnt, funct, false);
 }
 
-function move(destination) {
-  console.log(player.x, player.y);
-  stationary = false;
-  game.add.tween(player).to(destination, SPEED, Phaser.Easing.Quadratic.InOut,  true).onComplete.add(function() { stationary = true;}, this);
+saltydog = new MazeGame(data['grid']);
+
+
+
+function Coordinate(x, y) {
+  this.x = x;
+  this.y = y;
 }
 
-function debug() {
-  if (savePlayerGrid === null) {
-    savePlayerGrid = playerGrid;
-  }
-  else {
-    // console.log("savePlayerGrid ", savePlayerGrid.x);
-  }
-  if (savePlayer === null) {
-    savePlayer = player;
-  }
-  else {
-    // console.log("playerGrid ", playerGrid.x);
-  }
+Coordinate.prototype.direction = { "up": new Coordinate(0, -1),
+                                    "down": new Coordinate(0, 1),
+                                    "left": new Coordinate(-1, 0),
+                                    "right": new Coordinate(1, 0) };
 
-  if (playerGrid.x != savePlayerGrid.x || playerGrid.y != savePlayerGrid.y) {
-    console.log("playerGrid = ", playerGrid.x, " ", playerGrid.y);
-    savePlayerGrid = playerGrid;
-  }
-  if (player.x != savePlayer.x || player.y != savePlayer.y) {
-    console.log("player = ", player.x, " ", player.y);
-    savePlayer = player;
+Coordinate.prototype.increment = function (other) { this.x += other.x; this.y += other.y; };
+Coordinate.prototype.sum = function (other) { return new Coordinate(this.x = other.x, this.y + other.y) };
+
+function Player(location, startLocation, sprite) {
+  this.location = location;
+  this.startLocation = startLocation;
+  this.sprite = sprite;
+  this.isMoving = false;
+}
+
+function getNeighbors(point, val, maze) {
+  neighbors = [];
+  for (key in Coordinate.prototype.check) {
+    if (point.check[key](val, maze)) {
+      neighbors.push()
+    }
   }
 }
